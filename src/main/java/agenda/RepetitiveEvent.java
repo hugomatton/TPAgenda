fc4179f7b2bbab231ae0a45585c15a2cff1c1b4c
Lise Gauthier <lise@MacBook-Air-de-Lise.local>
2021-12-17 17:44:00 +0100

    final push

--- src/main/java/agenda/RepetitiveEvent.java
+++ src/main/java/agenda/RepetitiveEvent.java
@@ -77,23 +77,68 @@ public class RepetitiveEvent extends Event {
     public boolean isInDay(LocalDate aDay){
 
         LocalDate myStart = getStart().toLocalDate();
-        LocalDate dateSup = myStart.plus(0, frequency);
+        LocalDate dateSup = myStart;
+        System.out.println(dateSup);
 
         //on vérifie que le jour passé en paramètre est après le jour de début de l'event
-        if (aDay.isAfter(dateSup)|| aDay.equals(myStart)){
+        if (aDay.isAfter(dateSup) || aDay.equals(myStart)){
             //pour chaque jour où se produit l'event (1 fois/semaine), on vérifie si ce jour là correspond au jour passé en paramètre
-            while (aDay.isAfter(myStart)){
-                if (dateSup.equals(aDay)){
-                    if(!datesException.contains(aDay))
-                        return true;
+            while (aDay.isAfter(dateSup) || aDay.equals(dateSup)){
+                System.out.println("boucle");
+                
+                if (dateSup.equals(aDay) && (!datesException.contains(aDay))){
+                    return true;
                 }
-                dateSup.plus(1, frequency);
+                System.out.println("cc hugo");
+                dateSup=dateSup.plus(1, frequency);
+                System.out.println(dateSup);
+                
             }
+            System.out.println("sortie boucle");
         }
         return false;
     }
 
-  
+    
+    public static void main(String[] args){
+        // November 1st, 2020
+    LocalDate nov_1_2020 = LocalDate.of(2020, 11, 1);
+
+    // November 1st, 2020, 22:30
+    LocalDateTime nov_1__2020_22_30 = LocalDateTime.of(2020, 11, 1, 22, 30);
+
+    // 120 minutes
+    Duration min_120 = Duration.ofMinutes(120);
+
+    // A daily repetitive event, never ending
+    // November 1st, 2020, 22:30, 120 minutes
+    RepetitiveEvent neverEnding = new RepetitiveEvent("Never Ending", nov_1__2020_22_30, min_120, ChronoUnit.DAYS);
+
+    neverEnding.isInDay(nov_1_2020.plus(10, ChronoUnit.DAYS));
+    }
+    
+    
+    /*
+    public static void main(String[] args) {
+        LocalDate nov_1_2020 = LocalDate.of(2020, 11, 1);
+
+        // November 1st, 2020, 22:30
+        LocalDateTime nov_1__2020_22_30 = LocalDateTime.of(2020, 11, 1, 22, 30);
+    
+        // 120 minutes
+        Duration min_120 = Duration.ofMinutes(120);
+    
+        // A daily repetitive event, never ending
+        // November 1st, 2020, 22:30, 120 minutes
+        RepetitiveEvent neverEnding = new RepetitiveEvent("Never Ending", nov_1__2020_22_30, min_120, ChronoUnit.DAYS);
+
+        LocalDate myStart = neverEnding.getStart().toLocalDate();
+        LocalDate dateSup = myStart.plus(0, neverEnding.getFrequency());
+
+        System.out.println(dateSup);
+        System.out.println(dateSup.plus(10, neverEnding.getFrequency()));
+    }
+  */
 
     /**
      *
