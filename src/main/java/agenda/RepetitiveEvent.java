5e654293d4f165177ab865c44b863b6e6e948856
hugomatton <hugomattonisis@gmail.com>
2021-12-17 17:52:19 +0100

    finito

--- src/main/java/agenda/RepetitiveEvent.java
+++ src/main/java/agenda/RepetitiveEvent.java
@@ -43,36 +43,6 @@ public class RepetitiveEvent extends Event {
         return datesException;
     }
 
-    @Override
-    /*
-    public boolean isInDay2(LocalDate aDay){
-        boolean res = false;
-
-
-        //on fait une copie de l'objet courant pour ne pas le modifier
-        RepetitiveEvent event = new RepetitiveEvent(this.getTitle(), this.getStart(), this.getDuration(), frequency);
-        while(event.getStart().toLocalDate().isBefore(aDay)){
-            
-            long dureeMinutes = event.getDuration().toMinutes();
-            LocalDateTime myEnd = event.getStart().plus(dureeMinutes, ChronoUnit.MINUTES);
-
-            LocalDate jourFin = myEnd.toLocalDate();
-            LocalDate jourDebut = event.getStart().toLocalDate();
-
-            if (aDay.isBefore(jourFin) && aDay.isAfter(jourDebut)|| aDay.equals(jourDebut) || aDay.equals(jourFin)){
-                res = true;
-            }
-            //on incremente le debut de l'evenement par la frequence
-            event.setStart(event.getStart().plus(1,frequency));
-        }
-        
-        //on regarde que le jour en parametre n'est pas un jour exception
-        if(datesException.contains(aDay)){
-            res = false;
-        }
-        return res;
-    }
-    */
 
     public boolean isInDay(LocalDate aDay){
 
@@ -84,61 +54,20 @@ public class RepetitiveEvent extends Event {
         if (aDay.isAfter(dateSup) || aDay.equals(myStart)){
             //pour chaque jour où se produit l'event (1 fois/semaine), on vérifie si ce jour là correspond au jour passé en paramètre
             while (aDay.isAfter(dateSup) || aDay.equals(dateSup)){
-                System.out.println("boucle");
                 
                 if (dateSup.equals(aDay) && (!datesException.contains(aDay))){
                     return true;
                 }
-                System.out.println("cc hugo");
                 dateSup=dateSup.plus(1, frequency);
-                System.out.println(dateSup);
                 
             }
-            System.out.println("sortie boucle");
         }
         return false;
     }
 
     
-    public static void main(String[] args){
-        // November 1st, 2020
-    LocalDate nov_1_2020 = LocalDate.of(2020, 11, 1);
-
-    // November 1st, 2020, 22:30
-    LocalDateTime nov_1__2020_22_30 = LocalDateTime.of(2020, 11, 1, 22, 30);
-
-    // 120 minutes
-    Duration min_120 = Duration.ofMinutes(120);
-
-    // A daily repetitive event, never ending
-    // November 1st, 2020, 22:30, 120 minutes
-    RepetitiveEvent neverEnding = new RepetitiveEvent("Never Ending", nov_1__2020_22_30, min_120, ChronoUnit.DAYS);
-
-    neverEnding.isInDay(nov_1_2020.plus(10, ChronoUnit.DAYS));
-    }
     
     
-    /*
-    public static void main(String[] args) {
-        LocalDate nov_1_2020 = LocalDate.of(2020, 11, 1);
-
-        // November 1st, 2020, 22:30
-        LocalDateTime nov_1__2020_22_30 = LocalDateTime.of(2020, 11, 1, 22, 30);
-    
-        // 120 minutes
-        Duration min_120 = Duration.ofMinutes(120);
-    
-        // A daily repetitive event, never ending
-        // November 1st, 2020, 22:30, 120 minutes
-        RepetitiveEvent neverEnding = new RepetitiveEvent("Never Ending", nov_1__2020_22_30, min_120, ChronoUnit.DAYS);
-
-        LocalDate myStart = neverEnding.getStart().toLocalDate();
-        LocalDate dateSup = myStart.plus(0, neverEnding.getFrequency());
-
-        System.out.println(dateSup);
-        System.out.println(dateSup.plus(10, neverEnding.getFrequency()));
-    }
-  */
 
     /**
      *
